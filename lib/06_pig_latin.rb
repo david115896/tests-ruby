a23ee5880267ede1da18b750754b9127966516b9
david115896 <dtaccori@gmail.com>
2019-10-07 23:33:01 +0200

    all done

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,38 +1,11 @@
 def translate(word)
-	word.split.map{|x| if x[0].match?(/[aeiou]/)==false then x[1,x.length]+x[0]+"ay"end}.join('')
+	word.gsub("qu","*").split.map{|x| if x[0].match?(/[aeiou]/)==false && x[1].match?(/[aeiou]/)==false && x[2].match?(/[aeiou]/)==false then x[3,x.length]+x[0,3]+"ay"   elsif x[0].match?(/[aeiou]/)==false && x[1].match?(/[aeiou]/)==false then x[2,x.length]+x[0,2]+"ay"	elsif x[0].match?(/[aeiou]/)==false then x[1,x.length]+x[0]+"ay" else x+"ay"  end}.join(' ').gsub("*","qu")
 	
 		
 end
  
-def translte(str)
-	str.split.map { |x|
-	if (x[0] =~ /[aeiouy]/i) then x + "ay"
-	elsif (x[0] =~ /[[^aeiouy]]/i && x[1] =~ /[[aeiouy]]/i && x[0..1] != "qu") then x[1..x.length] + x[0] + "ay"
-	elsif (x[0..1] =~ /[[^aeiouy]]/i && x[2] =~ /[[aeiouy]]/i && x[1..2] != "qu") then x[2..x.length] + x[0..1] + "ay"
-	elsif (x[0..2] =~ /[[^aeiouy]]/i) then x[3..x.length] + x[0..2] + "ay"
-	end}.join(' ')
-end
-
-def essay
-	if word[0].include?(/[aeiouy]/)
-           word=word[1,long]+word[0]+"ay"
-	   word="1"	
-	 elsif word[0]=~/[aeiouy]/ && word[1]=~/[aeiouy]/
-
-	  word=word[2,long]+word[0,1]+"ay"
-	 word="2"
-	 else
-	  word=word+"ay"
-	word="3"
-	end
-
-end
-
 def s(word)
 return word
 end
 
-
-puts translate("apple")
-puts translate("tbbay iepay" )
 
